84a0cad09f74f7fa030b3233b77d92734c74f9db
Yi-Lin-Lai <ts03085781@gmail.com>
2025-06-08 10:25:03 +0800

    chore: add ant design v5 patch for react 19

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import '@ant-design/v5-patch-for-react-19';
 import './index.css';
 import App from './App.tsx';
 import './i18n';
