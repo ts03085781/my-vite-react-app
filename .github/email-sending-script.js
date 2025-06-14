c8082d965cfe6ebb7cc9cbd0ea52071a67fcac24
Dean <ts03085781@gmail.com>
2025-06-14 19:37:58 +0800

    fix: fix sending email action

--- .github/email-sending-script.js
+++ .github/email-sending-script.js
@@ -1,5 +1,6 @@
-const sgMail = require('@sendgrid/mail');
+#! /usr/bin/env node
 
+const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const msg = {
@@ -7,14 +8,10 @@ const msg = {
   from: 'ts03085781@gmail.com',
   subject: '🚨 Main 分支有新的 push！',
   text: '有人對 main 分支做了 push。',
+  html: '<p>有人對 main 分支做了 push。</p>',
 };
 
 sgMail
   .send(msg)
-  .then(() => {
-    console.log('Email 寄件成功!');
-  })
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+  .then(() => console.log('Mail sent successfully'))
+  .catch((error) => console.error(error.toString()));
