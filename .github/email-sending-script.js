9077357164b71498819fa2336a4d64a41750f146
Dean <ts03085781@gmail.com>
2025-06-14 21:05:23 +0800

    feat: add more information of git detail when send email

--- .github/email-sending-script.js
+++ .github/email-sending-script.js
@@ -6,12 +6,32 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
   to: 'ts03085781@gmail.com',
   from: 'ts03085781@gmail.com',
-  subject: '🚨 Main 分支有新的 push！',
-  text: '有人對 main 分支做了 push。',
-  html: '<p>有人對 main 分支做了 push。</p>',
+  subject: `🚨 github ${process.env.GITHUB_REPOSITORY} 專案中的 Main 分支有新的 push！`,
+  text: `
+有人對 Main 分支做了 push。
+
+專案名稱：${process.env.GITHUB_REPOSITORY}
+提交者：${process.env.GITHUB_ACTOR}
+提交時間：${new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' })}
+提交 SHA：${process.env.GITHUB_SHA}
+提交訊息：${process.env.GITHUB_EVENT_PATH ? require(process.env.GITHUB_EVENT_PATH).head_commit.message : '無法獲取'}
+查看變更：https://github.com/${process.env.GITHUB_REPOSITORY}/commit/${process.env.GITHUB_SHA}
+  `,
+  html: `
+    <h2>🚨 GitHub 專案更新通知</h2>
+    <p>有人對 Main 分支做了 push。</p>
+    <ul>
+      <li><strong>專案名稱：</strong>${process.env.GITHUB_REPOSITORY}</li>
+      <li><strong>提交者：</strong>${process.env.GITHUB_ACTOR}</li>
+      <li><strong>提交時間：</strong>${new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' })}</li>
+      <li><strong>提交 SHA：</strong>${process.env.GITHUB_SHA}</li>
+      <li><strong>提交訊息：</strong>${process.env.GITHUB_EVENT_PATH ? require(process.env.GITHUB_EVENT_PATH).head_commit.message : '無法獲取'}</li>
+    </ul>
+    <p><a href="https://github.com/${process.env.GITHUB_REPOSITORY}/commit/${process.env.GITHUB_SHA}">點擊查看變更內容</a></p>
+  `,
 };
 
 sgMail
   .send(msg)
-  .then(() => console.log('Mail sent successfully'))
-  .catch((error) => console.error(error.toString()));
+  .then(() => console.log('郵件發送成功'))
+  .catch((error) => console.error('郵件發送失敗：', error.toString()));
